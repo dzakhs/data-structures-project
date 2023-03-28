a35efc33f4e0e089aa60ef59003d5456d32847ef
Dmitry Zakhs <d.zakhs@gmail.com>
2023-03-29 00:29:38 +0300

    add homework 4

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -11,9 +11,20 @@ class TestQueue(unittest.TestCase):
     def test_enqueue(self):
         item = Queue()
         item.enqueue('data')
-        self.assertEqual(self.head, 'data')
-        self.assertEqual(self.tail, 'data')
+        self.assertEqual(item.head.data, 'data')
+        self.assertEqual(item.tail.data, 'data')
+
+    def test_dequeue(self):
+        item =Queue()
+        item.enqueue('data1')
+        item.enqueue('data2')
+        self.assertEqual(item.dequeue(), 'data1')
+        item.dequeue()
+        self.assertIsNone(item.dequeue())
 
 
     def test__str__(self):
-        pass
+        item = Queue()
+        item.enqueue('data1')
+        item.enqueue('data2')
+        self.assertEqual(str(item), "data1\ndata2")
